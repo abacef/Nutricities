dc32f08de6f437f14c0fd4ce7a028e84ae6119aa
Daniel Boaitey <boaiteyd7@gmail.com>
2018-02-10 21:08:54 -0500

    transition

--- README.md
+++
@@ -1,1 +0,0 @@
-# Nutricities
--- src/main/java/Application.java
+++
@@ -1,8 +0,0 @@
-import static spark.Spark.get;
-
-public class Application {
-    public static void main(String[] args) {
-        get("/home", (req, res) -> "Hello World");
-        System.out.println("home loaded: 0.0.0.0.4567");
-    }
-}
