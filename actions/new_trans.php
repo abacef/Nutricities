1e9df9ab4100a5ed75aa6c986ac0e4e87a29a523
Mark Nash <abacefabmicroblast@hotmail.com>
2018-02-11 00:08:51 -0500

    Done with php getting of the resturant name

--- actions/new_trans.php
+++ actions/new_trans.php
@@ -1,20 +1,17 @@
 <?php
-//date, merchant, amount and token sent to CreateTransaction command
-$date = urlencode($_POST['date']);
-$merchant = urlencode($_POST['merchant']);
-$amount = urlencode($_POST['amt']);
-$token = urlencode($_POST['token']);
-$partName = 'applicant';
-$method1 = 'GET';
-$url = 'https://www.expensify.com/api?command=CreateTransaction&authToken=' . $token . '&created=' . $date . '&amount=' . $amount .'&merchant=' . $merchant;
-
-$curl2 = curl_init($url);
-curl_setopt($curl2, CURLOPT_TIMEOUT, 5);
-curl_setopt($curl2, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($curl2, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($curl2);
-curl_close($curl2);
-
+$ch = curl_init();
+$data = "Rochester, NY, USA";
+$data = rawurlencode($data);
+$url = "https://developers.zomato.com/api/v2.1/cities?q=" . $data;
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+$headers = array();
+$headers[] = "Accept: application/json";
+$headers[] = "User-Key: 0a612313c1bc06055c4146d2e5532cae";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}                                                                                                                                     curl_close ($ch);
 echo $result;
-
-?>
+?>  
