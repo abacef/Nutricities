eee5994f1bbda0e9a34c0339fd5e773db7661e41
Daniel Boaitey <boaiteyd7@gmail.com>
2018-02-10 20:48:02 -0500

    Current version

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,7 @@ import static spark.Spark.get;
 
 public class Application {
     public static void main(String[] args) {
-        get("/home", new GetHomeRoute());
+        get("/home", (req, res) -> "Hello World");
         System.out.println("home loaded: 0.0.0.0.4567");
     }
 }
--- src/main/java/GetHomeRoute.java
+++ src/main/java/GetHomeRoute.java
@@ -1,11 +1,21 @@
+import com.google.maps.model.AutocompletePrediction;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import com.google.maps.*;
 
 
 public class GetHomeRoute implements Route {
 
     public Object handle(Request request, Response response) throws Exception {
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyDEptNYc6WNs8Uikp8QSZeaG9dk-XjoQGY")
+                .build();
+        PlaceAutocompleteRequest req = new PlaceAutocompleteRequest(context) {};
+        AutocompletePrediction[] set_curr = req.input("New York City").await();
+        System.out.println(set_curr);
+
+
         return null;
     }
 }
